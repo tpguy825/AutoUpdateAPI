206dcee561e4de032587840671e65bcffcd3998b
tpguy825 <75158953+tpguy825@users.noreply.github.com>
2022-08-03 17:58:47 +0100

    updates

---
+++ src/Main.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace tpguy825\AutoPluginUpdater;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\plugin\Plugin;
+use Exception;
+use tpguy825\AutoPluginUpdater\tasks\UpdaterTask;
+
+class Main extends PluginBase{
+    /** @var array<array<string>> $downloads Array of plugins to download */
+    public array $downloads = [];
+    public string $version = "0.0.1";
+    public static Main $instance;
+
+    public function onEnable(): void {
+        self::$instance = $this;
+		//if ($this->config->isCheckUpdate()) {
+            $plugins = $this->getServer()->getPluginManager()->getPlugins();
+            for($x = 0; $x <= count($plugins); $x++) {
+                $this->downloads[$x] = ["name" => $plugins[$x]->getName(), "version" => $plugins[$x]->getDescription()->getVersion()];
+            }
+			try {
+				$this->getServer()->getAsyncPool()->submitTask(new UpdaterTask);
+                $this->getLogger()->info("Checking for updates...");
+			} catch (Exception $e) {
+				$this->getLogger()->warning("Unable to start AutoPluginUpdater task: ".$e->getMessage());
+			}
+		//}
+	}
+}
---
+++ src/tasks/UpdaterTask.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tpguy825\AutoPluginUpdater\tasks;
+
+use Exception;
+use tpguy825\AutoPluginUpdater\Main;
+use pocketmine\scheduler\AsyncTask;
+use pocketmine\utils\Internet;
+use Phar;
+use pocketmine\utils\InternetRequestResult;
+use function yaml_parse;
+
+class UpdaterTask extends AsyncTask {
+	private Main $main;
+
+	/**
+	 * @throws Exception
+	 */
+	public function onRun(): void {
+		$this->main = Main::$instance;
+		$downloads = $this->main->downloads;
+		for($x = 0; $x <= count($downloads); $x++) {
+			$result = Internet::getURL("https://poggit.pmmp.io/get/".$downloads[$x]["name"], 10, ["User-Agent: AutoPluginUpdater by tpguy825 v".$this->main->version.", php ".PHP_VERSION]);
+			if($result instanceof InternetRequestResult) {
+				if ($result->getCode() !== 200) {
+					$this->main->getLogger()->warning("Could not download ".$downloads[$x]["name"].": ".$result->getCode());
+				}
+				try {
+					file_put_contents($this->main->getDataFolder()."/../../plugins/".$downloads[$x]["name"].".phar", $result->getBody());
+					$pluginphar = new Phar($this->main->getDataFolder()."/../../plugins/".$downloads[$x]["name"].".phar");
+					$pluginphar->extractTo($this->main->getDataFolder(), "plugin.yml");
+					try {
+						try {
+							$pluginyml = file_get_contents($this->main->getDataFolder()."/plugin.yml");
+							if($pluginyml === false) {
+								$this->main->getLogger()->warning("Could not read plugin.yml.");
+								$pluginyml = "version: \"Error, check console\"";
+							}
+						} catch (Exception $e) {
+							$this->main->getLogger()->warning("Could not read plugin.yml: ".$e->getMessage());
+							$pluginyml = "version: \"Error, check console\"";
+						}
+						$pluginyml = yaml_parse($pluginyml);
+						/**
+						 * @var array<string> $pluginyml 
+						 * @var string $pluginversion
+						*/
+						$pluginversion = $pluginyml["version"];
+						if ($pluginversion > $downloads[$x]["version"]) {
+							$this->main->getLogger()->warning("Plugin ".$downloads[$x]["name"]." is outdated! (".$pluginversion." > ".$downloads[$x]["version"].") Updating...");
+						} else {
+							$this->main->getLogger()->info("Plugin ".$downloads[$x]["name"]." is up to date!");
+						}
+					} catch (Exception $e) {
+						$this->main->getLogger()->warning("Could not parse plugin.yml for ".$downloads[$x]["name"].": ".$e->getMessage());
+						$pluginversion = null;
+					}
+					$downloads[$x] = ["name" => $downloads[$x], "version" => $pluginversion];
+				} catch (Exception $e) {
+					$this->main->getLogger()->warning("Could not write ".$downloads[$x]["name"].": ".$e->getMessage());
+					$downloads[$x] = ["name" => $downloads[$x], "error" => $e->getMessage()];
+				}
+			} else {
+				$this->main->getLogger()->warning("Could not download ".$downloads[$x]["name"].": Internet::getURL returned null and not InternetRequestResult");
+				$downloads[$x] = ["name" => $downloads[$x], "error" => "result is null"];
+			}
+		}
+		$this->setResult($downloads);
+	}
+
+	public function onCompletion(): void {
+		$this->main->getLogger()->info("Successfully updated plugins!");
+	}
+}
